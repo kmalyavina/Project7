b3d6b860a1c5b6b683542960c6e377d41e239f75
kmalyavina <kmalyavina@gmail.com>
2016-11-18 14:26:04 -0600

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,12 @@
+package assignment7;
+
+import java.util.List;
+
+public class Client {
+	private String userName;
+	private String password;
+	private String IP;
+	private List<Client> friends;
+	
+
+}
---
+++ ServerMain.java
@@ -0,0 +1,15 @@
+package assignment7;
+
+import java.util.List;
+
+public class ServerMain {
+	
+	protected List<Client> onlineUsers;
+	protected List<Client> registered;
+	
+	
+	public static void main(String[] args){
+		
+	}
+	
+}
