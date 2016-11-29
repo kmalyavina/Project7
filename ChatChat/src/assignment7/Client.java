01989956af850402655d4d69ed9bcee46e1bead9
Katya <kmalyavina@gmail.com>
2016-11-28 18:58:51 -0600

    User file

--- ChatChat/src/assignment7/Client.java
+++ ChatChat/src/assignment7/Client.java
@@ -37,24 +37,6 @@ import javafx.stage.Stage;
 
 public class Client extends Application {
 
-	public class User{
-		protected String userName;		// username(cannot change)(key)
-		protected String password;		// password for logging in (changable)
-		protected String displayName;	// displayed name (changable)
-		protected Image avatar;			// icon (changable)
-		protected List<User> friends;	// list of friends
-		protected boolean status;		// online/offline
-		
-		public User(String name, String pass, String nickname, Image img){
-			userName = name;
-			password = pass;
-			displayName = nickname;
-			avatar = img;
-			friends = new ArrayList<User>();
-			status = true;
-		}
-	}
-
 		private User user;
 		private static ObjectOutputStream toServer = null;
 		private static ObjectInputStream fromServer = null;
@@ -134,4 +116,33 @@ public class Client extends Application {
 	        primaryStage.show();
 			
 		}
+}
+
+class User{
+	protected String userName;		// username(cannot change)(key)
+	protected String password;		// password for logging in (changable)
+	protected String displayName;	// displayed name (changable)
+	protected String avatar;			// icon (changable)
+	protected List<String> friends;	// list of friends
+	protected boolean status;		// online/offline
+	
+	// constructor when have all info
+	public User(String name, String nickname, String pass, String img, Boolean s, ArrayList<String> fl){
+		userName = name;
+		password = pass;
+		displayName = nickname;
+		avatar = img;
+		friends = fl;
+		status = s;
+	}
+	
+	// constructor for NEW user
+	public User(String name,String nickname, String pass, String img){
+		userName = name;
+		password = pass;
+		displayName = nickname;
+		avatar = img;
+		friends = new ArrayList<String>();
+		status = true;
+	}
 }
